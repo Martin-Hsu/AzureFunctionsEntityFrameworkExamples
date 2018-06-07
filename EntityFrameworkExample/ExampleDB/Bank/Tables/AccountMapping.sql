2fab501493d937267ce5a72cb56f5f9a8df77041
mac-mini <noiseyou99.cs00g@g2.nctu.edu.tw>
2018-06-07 16:59:38 +0800

    改成為自己要開發的部分

--- EntityFrameworkExample/ExampleDB/Bank/Tables/AccountMapping.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Bank].[AccountMapping]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [UserId] INT NOT NULL, 
-    [AccountId] INT NOT NULL, 
-    CONSTRAINT [FK_AccountMapping_ToUser] FOREIGN KEY ([UserId]) REFERENCES [Bank].[User]([Id]),
-    CONSTRAINT [FK_AccountMapping_ToAccount] FOREIGN KEY ([AccountId]) REFERENCES [Bank].[Account]([Id])
-)
