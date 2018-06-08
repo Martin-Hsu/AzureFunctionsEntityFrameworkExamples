233a4dc1a60151c44deb31b0c27d476bb50e4418
mac-mini <noiseyou99.cs00g@g2.nctu.edu.tw>
2018-06-08 09:29:28 +0800

    完成sendline功能

--- chtline_azure_function/chtline_azure_function/SendLine.cs
+++ chtline_azure_function/chtline_azure_function/SendLine.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 
 namespace chtline_azure_function
 {
@@ -40,51 +41,107 @@ namespace chtline_azure_function
             }
             return req.CreateResponse(HttpStatusCode.OK);
         }
+    }
 
-
-        /// <summary>
-        /// 舊的專案 使用 LineInfo
-        /// </summary>
-        public class LineInfo
+    public static class SendLineWithPic
+    {
+        [FunctionName("SendLine/WithPic")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "SendLine/WithPic")]HttpRequestMessage req, TraceWriter log)
         {
-            public string UserID { set; get; }
-            public string Message { set; get; }
-            public LineInfo(string _userid, string _message)
+            string postData = req.Content.ReadAsStringAsync().Result;
+            LineInfoWithPic info = JsonConvert.DeserializeObject<LineInfoWithPic>(postData);
+            string[] userids = info.UserID.Split(';');
+
+            log.Info("postData :" + postData);
+
+            log.Info("info.Message :" + info.Message);
+            if (info.Message != "")
             {
-                UserID = _userid;
-                Message = _message;
+                foreach (string userid in userids)
+                {
+                    if (userid.Trim() != "")
+                    {
+                        try
+                        {
+                            log.Info("userid :" + userid);
+                            isRock.LineBot.Utility.PushMessage(userid, info.Message, info.ChannelAccessToken);
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Error("info.Message Error:" + ex);
+                        }
+                    }
+                }
             }
+
+            log.Info("info.AccessImagePath:" + info.AccessImagePath);
+            if (info.AccessImagePath != "")
+            {
+                Uri ua = new Uri(info.AccessImagePath);
+                string en_url = ua.AbsoluteUri;
+                foreach (string userid in userids)
+                {
+                    if (userid.Trim() != "")
+                    {
+                        try
+                        {
+                            isRock.LineBot.Utility.PushImageMessage(userid, en_url, en_url, info.ChannelAccessToken);
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Error("info.AccessImagePath Error:" + ex);
+                        }
+                    }
+                }
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+
+
+    /// <summary>
+    /// 舊的專案 使用 LineInfo
+    /// </summary>
+        public class LineInfo
+    {
+        public string UserID { set; get; }
+        public string Message { set; get; }
+        public LineInfo(string _userid, string _message)
+        {
+            UserID = _userid;
+            Message = _message;
+        }
+    }
+
+    public class LineInfoWithPic
+    {
+        public string UserID { set; get; }
+        public string Message { set; get; }
+
+        public string Account { set; get; }
+
+        public string UpLayerName { set; get; }
+
+        public int LogID { set; get; }
+
+        public string tag { set; get; }
+
+        public string AccessImagePath { set; get; }
+
+        public string ChannelAccessToken { set; get; }
+
+        public LineInfoWithPic(string _userid, string _message)
+        {
+            UserID = _userid;
+            Message = _message;
         }
 
-        public class LineInfoWithPic
+        public string toString()
         {
-            public string UserID { set; get; }
-            public string Message { set; get; }
+            string result = "";
 
-            public string Account { set; get; }
-
-            public string UpLayerName { set; get; }
-
-            public int LogID { set; get; }
-
-            public string tag { set; get; }
-
-            public string AccessImagePath { set; get; }
-
-            public string ChannelAccessToken { set; get; }
-
-            public LineInfoWithPic(string _userid, string _message)
-            {
-                UserID = _userid;
-                Message = _message;
-            }
-
-            public string toString()
-            {
-                string result = "";
-
-                return result;
-            }
+            return result;
         }
     }
 }
